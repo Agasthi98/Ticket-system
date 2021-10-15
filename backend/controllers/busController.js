319dd074312b40e21a352cd6d1ae00e5088de423
Madush99 <madusankagajadeera99@gmail.com>
2021-10-16 00:36:31 +0530

    Trip history

--- backend/controllers/busController.js
+++ backend/controllers/busController.js
@@ -2,23 +2,32 @@ import asyncHandler from 'express-async-handler'
 import Bus from '../models/busModel.js'
 
 const insertBus = asyncHandler(async (req, res) => {
+  const { busId, busStation, price } = req.body
 
-    const { busId, busStation, price } = req.body
+  const newBus = new Bus({
+    user: req.user._id,
+    busId,
+    busStation,
+    price,
+  })
 
-
-    const newBus = new Bus({
-        user:req.user._id,
-        busId,
-        busStation,
-        price,
-    })
-    
-    try {
-          await newBus.save()
-          res.send('New bus added')
-    } catch (error) {
-          return res.status(400).json({ error });
-    }
+  try {
+    await newBus.save()
+    res.send('New bus added')
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
 })
 
-export { insertBus }
+const getUserTrips = asyncHandler(async (req, res) => {
+  const { userid } = req.body
+
+  try {
+    const trips = await Bus.find({ userid: userid }).sort({ _id: -1 })
+    res.send(trips)
+  } catch (error) {
+    return res.status(400).json({ message: 'error' })
+  }
+})
+
+export { insertBus, getUserTrips }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
-import './App.css';
+import './App.css'
 import Header from './components/Header/Header'
-import LoginScreen from './screens/Login/LoginScreen';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
-import SignupScreen from './screens/SignupScreen/SignupScreen';
-import UserProfile from './screens/UserProfile/UserProfile';
-import RechargeScreen from './screens/RechargeScreen/rechargeScreen';
-import RechargeFormScreen from './screens/RechargeFormScreen/rechargeFormScreen';
+import LoginScreen from './screens/Login/LoginScreen'
+import HomeScreen from './screens/HomeScreen/HomeScreen'
+import SignupScreen from './screens/SignupScreen/SignupScreen'
+import UserProfile from './screens/UserProfile/UserProfile'
+import RechargeScreen from './screens/RechargeScreen/rechargeScreen'
+import RechargeFormScreen from './screens/RechargeFormScreen/rechargeFormScreen'
 import BusScreen from './screens/BusScreen/BusScreen'
-import RecharbalanceScreen from './screens/RechargeBalance/recharbalanceScreen';
-import RechargehistoryScreen from './screens/RechargehistoryScreen/rechargehistoryScreen';
-import TripAmount from './screens/TripAmount/TripAmount';
+import RecharbalanceScreen from './screens/RechargeBalance/recharbalanceScreen'
+import RechargehistoryScreen from './screens/RechargehistoryScreen/rechargehistoryScreen'
+import TripAmount from './screens/TripAmount/TripAmount'
 
 const App = () => {
   return (
@@ -21,7 +21,10 @@ const App = () => {
       <Route path='/login' component={LoginScreen} />
       <Route path='/signup' component={SignupScreen} />
       <Route path='/profile' component={UserProfile} />
-      <Route path='/recharge/:rechargeAmount/:paymentMethod' component={RechargeScreen} />
+      <Route
+        path='/recharge/:rechargeAmount/:paymentMethod'
+        component={RechargeScreen}
+      />
       <Route path='/rechargeacc' component={RechargeFormScreen} />
       <Route path='/busIns' component={BusScreen} />
       <Route path='/balance' component={RecharbalanceScreen} />
@@ -31,4 +34,4 @@ const App = () => {
   )
 }
 
-export default App;
+export default App
