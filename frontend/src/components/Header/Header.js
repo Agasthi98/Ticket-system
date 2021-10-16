6e8396f3b9513d079c9da92ee938f480eebb42d9
Madush99 <madusankagajadeera99@gmail.com>
2021-10-16 17:05:52 +0530

    dfs

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -5,52 +5,54 @@ import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { logout } from '../../actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { userInfo } = userLogin
-
-    const logoutHandler = () => {
-        dispatch(logout())
-    }
-    return (
-        <header>
-            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-                <Container>
-                    <LinkContainer to='/'>
-                        <Navbar.Brand><span className='text-warning'>UK</span> Transports</Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='ml-auto'>
-                            <LinkContainer to='/signup'>
-                                <Nav.Link><i className='fas fa-shopping-cart'></i> Signup</Nav.Link>
-                            </LinkContainer>
-                            {userInfo ? (
-                                <NavDropdown title={userInfo.email} id='username'>
-                                    <LinkContainer to='/profile'>
-                                        <NavDropdown.Item >
-                                            Profile
-                                        </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <NavDropdown.Item onClick={logoutHandler}>
-                                        Logout
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            ) : (
-                                <LinkContainer to='/login'>
-                                    <Nav.Link> <i className='fas fa-user'>
-                                        </i>Sign In
-                                    </Nav.Link>
-                                </LinkContainer>
-                            )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/home'>
+            <Navbar.Brand>
+              <span className='text-warning'>UK</span> Transports
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/signup'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Signup
+                </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.email} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    {' '}
+                    <i className='fas fa-user'></i>Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header
