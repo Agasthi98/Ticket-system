cc2142500be2acfb27e138741688811e7e9b10ae
Agasthi98 <sankalana98@gmail.com>
2021-10-16 14:57:00 +0530

    updated

--- frontend/src/screens/BusScreen/BusScreen.js
+++ frontend/src/screens/BusScreen/BusScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { Form, Button, Col, Row, Tab, Nav, Container,Card } from 'react-bootstrap'
+import { Form, Button, Col, Row, Tab, Nav, Container, Card } from 'react-bootstrap'
 import KaduScreen from './177'
 import MoraScreen from './158'
+import { Link } from 'react-router-dom'
 
 
 const BusScreen = () => {
@@ -13,28 +14,32 @@ const BusScreen = () => {
         <>
 
             <Container className='mt-4' >
+                <h2 style={{color:'white', textAlign:'center'}}>Select Your Bus Route</h2>
+                <Link to='/balance' className='btn btn-dark my-3'>
+                    Go Back
+                </Link>
                 <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                    <Row>  
-                        <Col sm={3} style={{backgroundColor:'white'}}>
-                       
-                            <Nav variant='pills' className="flex-column">
+                    <Row>
+                        <Col sm={3} >
+
+                            <Nav className='nav' variant='pills' className="flex-column">
                                 <Nav.Item className='mt-4' >
                                     <Nav.Link eventKey="first">177 Kaduwela-Kollupitiya</Nav.Link>
                                 </Nav.Item>
-                               
-                                <Nav.Item className='mt-4'>
+
+                                <Nav.Item className='mt-4' >
                                     <Nav.Link eventKey="second">158 Moratuwa-Piliyandala</Nav.Link>
                                 </Nav.Item>
                             </Nav>
 
                         </Col>
-                        
+
                         <Col sm={9}>
                             <Tab.Content>
-                                <Tab.Pane eventKey="first" style={{color:'white'}}>
-                                    <KaduScreen  />
+                                <Tab.Pane eventKey="first" style={{ color: 'white' }}>
+                                    <KaduScreen />
                                 </Tab.Pane>
-                                <Tab.Pane eventKey="second" style={{color:'white'}}>
+                                <Tab.Pane eventKey="second" style={{ color: 'white' }}>
                                     <MoraScreen />
                                 </Tab.Pane>
                             </Tab.Content>
